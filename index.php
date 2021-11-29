c4a9b009264898428220d85b365afcf2836ceaa5
Leonid <alraun_work@mail.ru>
2021-11-29 18:57:24 +0300

    исправил замечания

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
-require_once './src/Task.php';
+
+require_once "vendor/autoload.php";
 
 $task = new Task(3);
 if($task->getNextStatus('cancel') === Task::STATUS_CANCELLED){echo 'Проверка 1 удалась' . '<br>';}
